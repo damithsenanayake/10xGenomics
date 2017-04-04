71f5b9c7dd116a76b351b338883ff6c8a597c622
damithsenanayake <dracusds123@gmail.com>
2017-04-04 10:56:11 +1000

    Adding megacell tute and downloader sh

---
+++ gbcm_test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+mat = np.loadtxt('gbcm.csv', delimiter=',')
+
+print mat.shape
---
+++ gbcm_test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /wehisan/home/allstaff/s/senanayake.d/
+
+python gbcm_test.py
